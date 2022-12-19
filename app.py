74ba78ace5bfd923f2dea4472b10e300c8f5e7a2
Emir <emiralic22@gmail.com>
2022-12-19 15:09:21 +0100

    Second update of the version

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app=Flask('__name__')
 
 @app.route('/')
+@app.route('/pocetna-stranica')
 def index():
-    return render_template('home.html')
+    return render_template('pocetna-stranica.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+
+
 @app.route('/youtube')
 def youtube():
     return render_template('youtube.html')
+
+@app.route('/prijava')
+def prijava():
+    return render_template('prijava.html')
+
+@app.route('/registracija')
+def registracija():
+    return render_template('registracija.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
